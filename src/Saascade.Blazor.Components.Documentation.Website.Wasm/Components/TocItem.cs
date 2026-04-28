3c019290c2093e4b453e20344d1364953a1b88b6
Pablo Carvalho <pablo.luan.carvalho@gmail.com>
2026-04-28 03:32:02 +0100

    fix: new design for filter and table of content fix

--- src/Saascade.Blazor.Components.Documentation.Website.Wasm/Components/TocItem.cs
+++ src/Saascade.Blazor.Components.Documentation.Website.Wasm/Components/TocItem.cs
@@ -1,3 +1,3 @@
 namespace Saascade.Blazor.Components.Documentation.Website.Wasm.Components;
 
-public record TocItem(string Id, string Text, int Level = 1);
+public record TocItem(string Id, string Text, int Level = 1, string Tech = "");
