3ef7f9078dcacd77d8a2fb86f40e8f6507c606d3
saascade <116745878+saascade@users.noreply.github.com>
2026-05-01 12:21:29 +0100

    g

---
+++ src/Saascade.Blazor.Components.DesignSystems/InternalSaascadeDesignSystems.cs
@@ -0,0 +1,14 @@
+﻿// namespace Saascade.Blazor.Components.Bootstrap4;
+//
+// public class InternalSaascadeDesignSystems
+// {
+//     public static readonly InternalSaascadeDesignSystems Instance = new ();
+//     private InternalSaascadeDesignSystems(){}
+//     
+//     public static class DarkElectricBlueTheme
+//     { 
+//         //v1
+//         //v2
+//         
+//     }
+// }
