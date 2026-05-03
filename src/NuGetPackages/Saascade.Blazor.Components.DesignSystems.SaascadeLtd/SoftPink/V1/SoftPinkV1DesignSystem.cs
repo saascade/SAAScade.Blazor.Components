624f0da3ae389003bef06d981efb5f6baf4fcfcf
saascade <116745878+saascade@users.noreply.github.com>
2026-05-03 16:18:14 +0100

    Started creating SoftPink theme

---
+++ src/NuGetPackages/Saascade.Blazor.Components.DesignSystems.SaascadeLtd/SoftPink/V1/SoftPinkV1DesignSystem.cs
@@ -0,0 +1,58 @@
+﻿using Humanizer;
+using Saascade.Blazor.Components.Aliases;
+using Saascade.Blazor.Components.Experimental; 
+
+namespace Saascade.Blazor.Components.DesignSystems.SaascadeLtd;
+ 
+public class SoftPinkV1DesignSystem : IDesignSystem
+{ 
+    public string DisplayName { get; } = nameof(SoftPinkV1DesignSystem).Replace("DesignSystem", "").Humanize();
+    public CssFramework CssFramework { get; } = CssFramework.SaascadeLtd;
+
+
+    public string? GetComponentName(BaseComponent component)
+        => component.GetType().Name switch  // component.GetStandardizedComponentName() ?? 
+        {
+            nameof(A) => "a",
+            nameof(H1) => "h1", 
+            nameof(H2) => "h2", 
+            nameof(H3) => "h3", 
+            nameof(H4) => "h4", 
+            nameof(H5) => "h5", 
+            _ => null
+        };
+
+    public string? GetSpecializations<T>(T component) where T : BaseComponent
+        => component.GetType().Name switch
+        {
+            nameof(PrimaryButton) => "btn-primary",
+            nameof(SecondaryButton) => "btn-secondary",
+            nameof(DestructiveButton) => "btn-error",
+            nameof(AcceptButton) => "btn-success",
+            nameof(SubmitButton) => "btn-primary",
+            nameof(CloseButton) => "btn-secondary",
+            nameof(CancelButton) => "btn-secondary",
+            nameof(DeleteButton) => "btn-error",
+            _ => null
+        };
+
+    public string? GetStyleForComponent<T>(T component) where T : BaseComponent
+        => component.GetType().Name switch
+        {
+            _ => null
+        };
+
+
+    public string[] GetStylesheetReferences() =>
+    [
+        """ <link rel="stylesheet" href="_content/Saascade.Blazor.Components.DesignSystems.SaascadeLtd/SoftPink/v1/css/styles.css" /> """,
+    ];
+
+    public string[] GetJavaScriptReferences() => [];
+    
+    public string[] GetFontReferences() =>
+    [
+        //TODO
+        """ TODO href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" """, 
+    ];
+}
